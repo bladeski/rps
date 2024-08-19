b4efe80603d5b04fe43658100f0c135aaedcd1b0
Jonathan Blades <jonoblades@gmail.com>
2024-08-19 11:58:45 +0100

    Fix build issues

--- svgo.config.cjs
+++ svgo.config.cjs
@@ -7,14 +7,7 @@ module.exports = {
           onlyMatchedOnce: false,
           removeMatchedSelectors: true
         }
-      },
-        {
-          name: "cleanupIds",
-          params: {
-            remove: true,
-            force: true
-          }
-        },
-      ],
+      }
+    ],
   };
   
